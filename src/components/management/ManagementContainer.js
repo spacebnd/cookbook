b74165e82a09ee06f0014e31316b5c32ea3c26db
Vitaly Petrovich <spacebndx@gmail.com>
2021-07-10 16:37:45 +0700

    add categories and ingredients search

--- src/components/management/ManagementContainer.js
+++ src/components/management/ManagementContainer.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Box } from '@material-ui/core'
 
 function ManagementContainer() {
-  return <div>ManagementContainer</div>
+  return <Box component="div">ManagementContainer</Box>
 }
 
 export default ManagementContainer
--- src/components/search/SearchContainer.js
+++ src/components/search/SearchContainer.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Box } from '@material-ui/core'
+import RecipesSearch from './RecipesSearch.js'
+import IngredientsSearch from './IngredientsSearch.js'
+import CategorySearch from './CategorySearch.js'
 
 function SearchContainer() {
-  return <div>SearchContainer</div>
+  return (
+    <Box component="div">
+      <RecipesSearch />
+      <IngredientsSearch />
+      <CategorySearch />
+    </Box>
+  )
 }
 
 export default SearchContainer
