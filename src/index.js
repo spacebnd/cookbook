7ff8733ab76cf1c227962d132ff013077d46f8e1
Vitaly Petrovich <spacebndx@gmail.com>
2021-10-03 15:53:48 +0700

    configure logrocket monitoring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import LogRocket from 'logrocket'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { theme } from './common/theme.js'
 
 import App from './App'
+
+LogRocket.init('6i1wys/cookbook')
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
